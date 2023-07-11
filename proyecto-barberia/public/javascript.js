469ce98169e0f48c7f0434e9b98f9396fe938111
jorge Lezcano <jorgeluislezcano1988@gmail.com>
2023-07-10 23:56:05 -0300

    subiendo repositorio

---
+++ proyecto-barberia/public/javascript.js
@@ -0,0 +1,106 @@
+
+ let linea = document.querySelectorAll('.linea');
+ let links = document.querySelectorAll('header nav ul li a');
+  // Agrega un evento de clic a todos los enlaces del menú
+ links.forEach(function(link) {
+     link.addEventListener('click', function(event) {
+         event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+
+         let target = document.querySelector(link.getAttribute('href')); // Obtiene el elemento de destino según el atributo href del enlace
+
+         // Desplaza la página suavemente hacia el elemento de destino
+         target.scrollIntoView({
+             behavior: 'smooth'
+         });
+     });
+ });
+ if (window.innerWidth <= 768) {
+  let lineas = document.querySelectorAll('.linea');
+ let hamburguesa = document.querySelector('.hamburguesa');
+ let menu = document.querySelector('nav ul');
+ 
+ hamburguesa.addEventListener('click', function() {
+   menu.classList.toggle('menu-activo');
+   hamburguesa.classList.toggle('active');
+  //  hamburguesa.style.opacity = '0';
+   
+ });
+
+//  let links = document.querySelectorAll('header nav ul li a');
+ 
+
+links.forEach(function(link) {
+   link.addEventListener('click', function(event) {
+     event.preventDefault();
+     menu.classList.remove('menu-activo');
+     hamburguesa.style.opacity = '1'
+     
+
+     let target = document.querySelector(link.getAttribute('href'));
+     target.scrollIntoView({
+       behavior: 'smooth'
+     });
+     
+     if (link.getAttribute('href') === '#footer' || '#acerca') {
+      lineas.forEach(function(linea) {
+      linea.style.backgroundColor = 'white';
+      })
+    } else {
+      lineas.forEach(function(linea) {
+      linea.style.backgroundColor = '';
+    });
+    }
+   });
+ });
+}
+
+ // Obtener referencia al botón "Pedir Turno"
+ const btnPedirTurno = document.querySelectorAll('.btn-pedir-turno');
+
+ // Agregar un evento click al botón
+ btnPedirTurno.forEach(function(btn){
+    btn.addEventListener('click', function() {
+     // Abrir el formulario en una nueva ventana o pestaña
+     window.open('form.html', '_blank');
+ });
+});
+// obscureciendo body al llegar al footer
+window.addEventListener('scroll', function() {
+    let footer = document.getElementById('footer');
+    let body = document.body;
+  
+    let footerOffset = footer.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight;
+  
+    if (footerOffset < windowHeight) {
+      body.classList.add('footer-visible');
+    } else {
+      body.classList.remove('footer-visible');
+    }
+  });
+
+  // envio de Formulario consulta
+    document.addEventListener('DOMContentLoaded', () => {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            fetch('/enviar-consulta', {
+              method: 'POST',
+              body: new FormData(form),
+            })
+              .then((response) => {
+                if (response.ok) {
+                  // Muestra la ventana emergente
+                  alert('Gracias por enviar tu consulta. Nos pondremos en contacto contigo pronto.');
+                  // Restablece el formulario
+                  form.reset();
+                }
+              })
+            .catch(error => {
+                console.log(error);
+            });
+        });
+    });
+
+    
---
+++ proyecto-barberia/server.js
@@ -0,0 +1,87 @@
+ const express = require('express');
+ const bodyParser = require('body-parser');
+ const nodemailer = require('nodemailer');
+ const path = require('path');
+
+ const app = express();
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(express.static(path.join(__dirname, './public')));
+ app.use(express.static("public"))
+
+ app.post('/enviar', (req, res) => {
+     const { nombre, apellido, email, celular, horario } = req.body;
+
+     // Configuración del transporte de correo electrónico con nodemailer
+     const transporter = nodemailer.createTransport({
+         service: 'Gmail',
+         auth: {
+             user: 'jorgeluislezcano1988@gmail.com', // Reemplaza con tu dirección de correo electrónico
+             pass: 'ymeputallhzqhend' // Reemplaza con tu contraseña
+         }
+     });
+
+     // Contenido del correo electrónico
+     const mailOptions = {
+         from: 'jorgeluislezcano1988@gmail.com', // Reemplaza con tu dirección de correo electrónico
+         to: 'jorgeluislezcano1988@gmail.com', // Reemplaza con la dirección de correo electrónico de destino
+         subject: 'Nuevo turno solicitado',
+         text: `Nombre: ${nombre} ${apellido}\nEmail: ${email}\nCelular: ${celular}\nHorario: ${horario}`
+     };
+
+     // Envío del correo electrónico
+     transporter.sendMail(mailOptions, (error, info) => {
+         if (error) {
+             console.log(error);
+             res.status(500).send('Ocurrió un error al enviar el correo electrónico');
+         } else {
+             console.log('Correo electrónico enviado:', info.response);
+             res.send('Gracias por solicitar un turno. Nos pondremos en contacto contigo pronto.');
+         }
+     });
+ });
+
+ app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname, 'index.html'));
+    
+     res.setHeader("Access-Control-Allow-Origin","*")
+     
+ });
+
+ app.listen(3000, () => {
+     console.log('Servidor Node.js iniciado en el puerto 3000');
+ });
+ // envio de consulta formulario index.html
+ app.post('/enviar-consulta', (req, res) => {
+    const { nombre, email, consulta } = req.body;
+  
+    // Configuración del transporte de correo electrónico con nodemailer
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'jorgeluislezcano1988@gmail.com', // Reemplaza con tu dirección de correo electrónico
+        pass: 'ymeputallhzqhend' // Reemplaza con tu contraseña
+      }
+    });
+  
+    // Contenido del correo electrónico
+    const mailOptions = {
+      from: 'jorgeluislezcano1988@gmail.com', // Reemplaza con tu dirección de correo electrónico
+      to: 'jorgeluislezcano1988@gmail.com', // Reemplaza con la dirección de correo electrónico de destino
+      subject: 'Nueva consulta recibida',
+      text: `Nombre: ${nombre}\nEmail: ${email}\nConsulta: ${consulta}`
+    };
+  
+    // Envío del correo electrónico
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send('Ocurrió un error al enviar el correo electrónico');
+      } else {
+        console.log('Correo electrónico enviado:', info.response);
+        res.sendStatus(200)
+        // res.json({ success: true });
+        res.send('Gracias por enviar tu consulta. Nos pondremos en contacto contigo pronto.');
+      }
+    });
+  });
+    
